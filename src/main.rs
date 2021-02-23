e6c498ad2fd90f3dd601148f236210cb3d132e8c
Mike Zeller <mike@mikezeller.net>
2021-02-22 20:57:55 -0500

    add another test and kick off ci!

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,12 @@ mod test {
     fn test_zoneid() {
         assert_eq!(zonename::getzoneid().expect("failed to get zoneid"), 0);
     }
+
+    #[test]
+    fn test_zonename() {
+        let zoneid = zonename::getzoneid().expect("failed to get zoneid");
+        let zonename = zonename::getzonenamebyid(zoneid).expect("failed to get zone name");
+
+        assert_eq!(&zonename, "global");
+    }
 }
